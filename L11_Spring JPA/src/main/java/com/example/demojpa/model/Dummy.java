1ab12cda67c649fed64ab1804af128c392eeb32c
Ashish.Nar <Linkedin@1234>
2022-09-12 12:40:57 +0530

    adding L11 code

---
+++ L11_Spring JPA/src/main/java/com/example/demojpa/model/Dummy.java
@@ -0,0 +1,7 @@
+package com.example.demojpa.model;
+
+import javax.persistence.Entity;
+
+//@Entity
+public class Dummy {
+}
---
+++ L11_Spring JPA/src/main/java/com/example/demojpa/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.example.demojpa.repository;
+
+import com.example.demojpa.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+}
